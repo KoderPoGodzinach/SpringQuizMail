b2432b44643715de57e89e6b2351b434036a91a4
Rafal Dziedzic <rafal-dziedzic@gazeta.pl>
2017-11-21 21:56:50 +0100

    email commit

---
+++ src/main/java/dziedzic/rafal/webQuiz/controller/StartController.java
@@ -0,0 +1,97 @@
+package dziedzic.rafal.webQuiz.controller;
+
+import dziedzic.rafal.webQuiz.dao.DaoQuestion;
+import dziedzic.rafal.webQuiz.dao.DaoUser;
+import dziedzic.rafal.webQuiz.model.Question;
+import dziedzic.rafal.webQuiz.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.*;
+import services.EmailServicesImpl;
+
+@Controller
+public class StartController {
+
+    @Autowired(required = false)
+    EmailServicesImpl emailServices;
+
+    @Autowired         //jeżeli ten obiekt będzie potrzebny to Spring go wstrzyknie
+    DaoUser daoUser;
+    @Autowired
+    DaoQuestion daoQuestion;
+
+    int questionCount = 0;
+    int userPoints = 0;
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String showInputForm(ModelMap modelMap) {
+        modelMap.addAttribute("user", new User());
+        return "startForm";  // przekazujemy postego usera, który ma nie ustawione parametry, będziemy poberali metodą
+        // post wartości uzytkownika a nastepnie zapisać tego użytkownika
+
+    }
+
+    @PostMapping(value = "/")
+    public String createUser(@ModelAttribute User user, ModelMap modelMap) {
+        modelMap.addAttribute("user", user);
+        daoUser.addUser(user);
+
+        System.out.println(user.toString());  // roboczo generownie usera
+
+        return "redirect:/question";
+    }
+
+    @RequestMapping(value = "/question", method = RequestMethod.GET)
+    public String question(ModelMap modelMap) {
+        modelMap.addAttribute("question", new Question());
+        modelMap.addAttribute("textQuestion", daoQuestion.showAll().get(questionCount).getQuestionText());
+
+        modelMap.addAttribute("answerA", daoQuestion.showAll().get(questionCount).answersA);
+        modelMap.addAttribute("answerB", daoQuestion.showAll().get(questionCount).answersB);
+        modelMap.addAttribute("answerC", daoQuestion.showAll().get(questionCount).answersC);
+
+        return "question";
+    }
+
+    @PostMapping(value = "/question")
+    public String getAnswer(@ModelAttribute Question question, ModelMap modelMap) {
+        modelMap.addAttribute("question", question);
+        String url = "";
+        if (daoQuestion.showAll().get(questionCount).getCorrectAnswer() == question.getUserAnswer()) {
+            userPoints++;
+        }
+        questionCount++;
+
+        if (questionCount < 3) {
+            url = "question";
+        } else {
+            url = "thankyou";
+        }
+
+
+        return "redirect:/" + url;
+    }
+
+
+    @RequestMapping(value = "/thankyou")
+    public String summary(ModelMap modelMap) {
+
+        String summary = "Uzyskales punktow: " + userPoints + " Gratuluje";
+
+        String thanks = "Dziekujemy za wypelnienie naszego testu " + daoUser.showAll().get(0).getName();
+
+        modelMap.addAttribute("summary", summary);
+        modelMap.addAttribute("thanks", thanks);
+
+        emailServices.sendMessage(daoUser.showAll().get(0).getEmail(),"Wynik testu","Dziekuje");
+
+        return "thankYou";
+    }
+
+
+
+
+
+
+}
---
+++ src/main/java/services/EmailServicesImpl.java
@@ -0,0 +1,30 @@
+package services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+@Component
+public class EmailServicesImpl {
+
+    @Autowired
+    JavaMailSender javaMailSender;
+
+    public void sendMessage(String to, String subject, String text) {
+
+        SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
+
+        simpleMailMessage.setTo(to);
+        simpleMailMessage.setSubject(subject);
+        simpleMailMessage.setText(text);
+
+        javaMailSender.send(simpleMailMessage);
+
+    }
+
+
+
+}
